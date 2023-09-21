937fc32f1938f4a0077ee1c09fd42596ca6c2e95
Felix Ringe <99140162+KensingtonOscupant@users.noreply.github.com>
2023-09-21 15:39:07 +0200

    clarified texts, added clipboard feature for notifying_party

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import sys
 import termios
 import tty
 import os
+import pyperclip
 
 def getch():
     """Get a single character from the user without the need to press enter."""
@@ -14,6 +15,12 @@ def getch():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
+
+def translate_entity():
+    if row["legal_type"] == 0:
+        return "natural person"
+    elif row["legal_type"] == 1.0:
+        return "legal entity"
 
 # Prompt the user for the data file path
 data_file_path = input("Enter the path to the project folder (e.g. /Users/gisbertgurke/Documents/Projekte/germanyinc_oc_clu/build/exe.macosx-10.9-universal2-3.10 ): ")
@@ -31,8 +38,8 @@ next_id = 1
 
 # Print a welcome message
 print("Welcome to the company data utility!")
-print("Press 'a' to insert 0 in the 'legal_type' column.")
-print("Press 'd' to insert 1 in the 'legal_type' column.")
+print("Press 'a' to classify the notfying party as a natural person.")
+print("Press 'd' to classify the notfying party as a legal entity.")
 print("Press 'b' to go back to the previous question.")
 print("Press 'q' to quit the utility.")
 
@@ -56,9 +63,9 @@ while current_index < len(data):
 
     # Print the notifying party and ask the user for input
     print(f"Notifying Party: {notifying_party}")
-    print(f"Legal Type: {legal_type}")
+    print(f"Legal Type: {translate_entity()}")
     print(f"OpenCorporates URL: {open_corporates_url}")
-    print("Press 'a' to insert 0, 'd' to insert 1, '1' to edit URL, or press Enter: ")
+    print("Press 'a' to classify as a natural person, 'd' to classify as a legal entity, or 'b' to go back to the previous question:")
 
     while True:
         user_input = getch().lower()
@@ -80,6 +87,11 @@ while current_index < len(data):
         row["legal_type"] = 0
     elif user_input == "d":
         row["legal_type"] = 1.0
+
+        # Copy the notifying_party to clipboard when 'd' is pressed
+        pyperclip.copy(notifying_party)
+        print(f"'notifying_party' copied to clipboard: {notifying_party}")
+
         url_input = input("Enter OpenCorporates URL (or press Enter to leave it blank): ")
         row["OpenCorporates URL"] = url_input
 
@@ -90,5 +102,3 @@ while current_index < len(data):
         writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
         writer.writeheader()
         writer.writerows(data)
-
-    print("Data written to file.")
